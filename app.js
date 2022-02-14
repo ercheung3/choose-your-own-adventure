00a93677f95100faa227095ee6ad8150086a4dfe
ercheung3 <home@Erics-MBP.fios-router.home>
2022-02-14 15:33:27 -0500

    Added garage and car functionality.

--- app.js
+++ app.js
@@ -33,13 +33,20 @@ function checkInv() {
     if(pInv.length == 0) console.log(`You have NOTHING in your inventory`);
     else console.log(`You have these items in your inventory: ${pInv.toString()}`);
 }
-
+/**
+ * @name hasItem
+ * @description Checks player's inventory for a specific item
+ * 
+ * @param {string} item Item to be checked
+ * 
+ * @return {boolean} true if found, false if not found
+ */
 function hasItem(item) {
         //Checks index (Could use for loop)
         let index = 0;
         for (const invItem of pInv) {
             //Check inventory for item and automatically uses
-            if(invItem.toUpperCase() == item.toUpperCase()) {
+            if(invItem.toLowerCase() == item.toLowerCase()) {
                 //Item found.
                 return true;
             }
@@ -73,12 +80,12 @@ function checkPrompt(statement,keyword) {
  * 
  * @returns {int} index
  */
-
 function indexValidChoice(statement,arrayPrompts) {
     //Returns -1 for "" or null
     if(statement == "" || statement == null) return -1;
     return (arrayPrompts.indexOf(statement.toLowerCase()));
 }
+
 /**
  * @name checkValidChoice
  * @description Checks the choice of user
@@ -108,6 +115,14 @@ function checkValidChoice(statement,arrayPrompts) {
     return 'invalid';
 }
 
+/**
+ * @name zomebieEncounter
+ * @description Function for zombie encounters
+ * 
+ * @param none;
+ * 
+ * @returns null;
+ */
 function zombieEncounter() {
     let zombieChoice = "";
     let zombieChoicePrompts = ['fight','run']
@@ -117,6 +132,14 @@ function zombieEncounter() {
     else if(zombieChoice == zombieChoicePrompts[1]) console.log("ADD AN ESCAPE MECHANIC");
 }
 
+/**
+ * @name prologue
+ * @description prologue function to set the scene and ask for name
+ * 
+ * @param none
+ * 
+ * @returns null
+ */
 function prologue() {
     name = prompt('What is your name? ');
     //console.log(`DISCLAIMER: PLEASE READ THE README.MD BEFORE PLAYING THIS GAME`);
@@ -141,18 +164,11 @@ function leaveHouse() {
     leaveHouse = leaveHouse.toLowerCase();
 
     //Choice for garage.
-    if(leaveHouse == leaveHousePrompts[0]) {
-        
-        console.log("Dead!")
-    }
-
+    if(leaveHouse == leaveHousePrompts[0]) enterGarage();
     //Choice for room.
-    else if(leaveHouse == leaveHousePrompts[1]) {
-        enterBedroom();
-    }
-
+    else if(leaveHouse == leaveHousePrompts[1]) enterBedroom();
      //Choice for outside.
-     if(leaveHouse == leaveHousePrompts[2]) {
+    else if(leaveHouse == leaveHousePrompts[2]) {
         console.log(`You open the door as slowly as possible but a zombie hiding in a bush a few feet away finds you!`);
         //Maybe INCLUDE combat against zombies?
         zombieEncounter();
@@ -197,6 +213,72 @@ function enterBedroom() {
     leaveHouse();
 }
 
+/**
+ * @name enterGarage
+ * @description Choices for the garage
+ * 
+ * @param none
+ * 
+ * @returns null
+ */
+function enterGarage() {
+    console.log(`You walk towards the garage and open the door. Turning on the light switch, you find your 2010 Toyota Camry and some tools.`);
+    let garageChoice = "";
+    let garageChoicePrompts = ['car','tools','inside'];
+    
+    let carChoice = "";
+    let carChoicePrompts = ['start','look'];
+    //Continues prompts until car is [start]ed or you go back [inside] the house.
+    do {
+        //Prompt for garage interactions
+        garageChoice = "";
+        while(checkValidChoice(garageChoice,garageChoicePrompts) == 'invalid') garageChoice = prompt(`Check [car], [tools], or go back [inside] your house? `);
+        garageChoice = garageChoice.toLowerCase();
+
+        //User selects [car] choice
+        if(garageChoice == garageChoicePrompts[0]) {
+            //Checks for car keys in inventory
+            //Has keys in inventory
+            if(hasItem('keys')) {
+                carChoice = checkCar();
+            }
+            //No key in inventory
+            else console.log(`You check you pockets and don't find the keys to the car. Maybe you left it somewhere else in the house.`);
+        }
+        //User selects [tools]
+        else if(garageChoice == garageChoicePrompts[1]) {
+            console.log(`It's just a bunch of rusty old tools on the rack that you haven't used since the end of fall.`);
+            console.log(`You question yourself if its right or wrong to try to put these in your small pockets.`)
+        }
+        //User selects [inside]
+        else if(garageChoice == garageChoicePrompts[2]) leaveHouse();
+    } while(carChoice !== carChoicePrompts[0] && garageChoice !== garageChoicePrompts[2]);
+}
+
+/**
+ * @name checkCar
+ * @description Checks the car (with keys or not) and returns the user's choice in the car
+ * 
+ * @param none
+ * 
+ * @returns {string} carChoice
+ */
+function checkCar() {
+    let carChoice = "";
+    let carChoicePrompts = ['start','look'];
+    while(checkValidChoice(carChoice,carChoicePrompts) == 'invalid') carChoice = prompt(`You remember that you picked up your keys from the bedroom, do you want to [start] the car and leave the house or [look] around the garage?`);
+        carChoice = carChoice.toLowerCase();
+
+    if(carChoice == carChoicePrompts[0]) {
+        console.log(`LEAVE THE GARAGE FUNCTION`);
+        return carChoice;
+    }
+    else {
+        console.log(`You leave the car and look around the garage again.`);
+        return carChoice;
+    }
+        
+}
 
 //Calls the main game
 do {
